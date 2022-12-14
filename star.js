4ee90a7ae7cc6ac1141246cfe5a31851437bd120
vahidinho110 <119563504+vahidinho110@users.noreply.github.com>
2022-12-14 18:08:24 +0330

    Add files via upload

---
+++ star.js
@@ -0,0 +1,84 @@
+
+
+
+
+//let pricelist=[1000,2000,3600,2650,4125]
+//for (let index = 0; index < pricelist.lenghth ; index++) {
+    //pricelist[index] = pricelist[index]*0.9;
+    
+//}
+//console.log(pricelist);
+
+//or (let index = 0; index < array.length; index++) {
+   // const element = array[index];
+    
+   // let jam=0;
+    //for (let index = 0; index <1000 ; index++) {
+       // if( ( index%3==0)|| (index%5==0)                                 )                       
+        
+       // jam=jam+index
+   // }
+   // console.log( jam)
+   //let number=prompt("enter your number");
+
+  // for (let index = 0; index <number.length; index++) {
+   //let char=number.substring(index,index+1);
+  // let newString="";
+//for (let i = 0; i < char; i++) {
+  //  newString+=char;
+    
+   //}
+   //console.log(newString);
+
+    
+   //}
+
+
+
+
+   //let number1=prompt( "enter num1");
+   //let number2=prompt( "enter num1");
+   //let number3=prompt( "enter num1");
+   //f0or (let index = 0; index <99{
+
+// let number= prompt( "enter number        " )
+
+// for (let index = 0; index < number.length; index++) {
+//   let number1=nmber.substring( )
+  
+// }
+    
+
+// for (let index = 1; index < 1500; index++) {
+//   if (index % 2 !=0) {
+
+//     console.log(index);
+    
+//   }
+  
+// }
+// let input= prompt();
+// let string="";
+// for (let index = input.length; index >= 0; index--) {
+//   string += input.substring(index , index-1);
+  
+// }
+
+
+// console.log(string);
+
+
+
+let input=prompt();
+for (let f = 1; f <= input; f++) {
+  let string=""
+for (let e = 1; e <=f; e++){
+ 
+ 
+ 
+ string +=e 
+ 
+ 
+}
+console.log(string)  
+}
